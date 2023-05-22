8944862dcad1f18b10e80db94a5227b373daa4c5
kira <kiravillagracia@yahoo.com>
2023-05-22 14:17:05 +0800

    added functions for setting data observer

--- app/src/main/java/com/kira/hilttutorial/main/MainActivity.kt
+++ app/src/main/java/com/kira/hilttutorial/main/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.kira.hilttutorial.main
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.kira.hilttutorial.R
+import com.kira.hilttutorial.main.user.UserState
 import com.kira.hilttutorial.main.user.UserViewModel
-
+import com.kira.hilttutorial.main.user.model.User
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -16,10 +20,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupObserver()
     }
 
     override fun onResume() {
         super.onResume()
         viewModel.getRandomUsers()
+    }
+
+    private fun setupObserver() {
+        lifecycleScope.launch {
+            viewModel.userState.collect {state ->
+                when (state){
+                    is UserState.ShowError -> {
+                        Log.e("Error: ", state.error.toString())
+                    }
+
+                    is UserState.SetResults -> {
+                        setupRecyclerView(state.userList)
+                    }
+
+                    else -> {
+                        Log.e("Error: ", state.toString())
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setupRecyclerView(userList: List<User>) {
+
     }
 }
