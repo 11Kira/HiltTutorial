45cfa60687576df712f26ac1ee22b527c91ce607
kira <kiravillagracia@yahoo.com>
2023-05-22 13:54:16 +0800

    updated gradle dependencies

--- app/src/main/java/com/kira/hilttutorial/main/MainActivity.kt
+++ app/src/main/java/com/kira/hilttutorial/main/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.kira.hilttutorial.main
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.kira.hilttutorial.R
+import com.kira.hilttutorial.main.user.UserViewModel
 
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: UserViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.getRandomUsers()
     }
 }
